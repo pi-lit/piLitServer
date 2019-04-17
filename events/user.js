75028be1d6d5d9bef3f47956134dc791259fb9b9
ispicer <iospicer@gmail.com>
2019-04-17 19:27:27 -0400

    Test

--- events/user.js
+++ events/user.js
@@ -91,6 +91,7 @@ function forwardCommand(req, socket) {
 	console.log('forward command:');
 	console.log(req);
 
+    //forward command error messages
 	if(!res) {
         errorRes.error = "no object was sent";
         socket.emit('command', errorRes);
